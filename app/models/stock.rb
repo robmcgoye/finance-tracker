87641eb01ad6959f50b9d1eec7ddce19896ad966
Rob McGoye <rob@mcgoye.com>
2021-12-24 20:51:35 -0500

    Added additional fields for usrs

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
-
+  #validates_uniqueness_of :ticker, :case_sensitive => false
   validates :name, :ticker, presence: :true
 
   def self.new_lookup(ticker_symbol)
@@ -10,14 +10,14 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      new(ticker: (ticker_symbol).downcase, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
       return nil
     end
   end
 
   def self.check_db(ticker_symbol)
-    where(ticker: ticker_symbol).first
+    where(ticker: (ticker_symbol).downcase).first
   end
 
 end
