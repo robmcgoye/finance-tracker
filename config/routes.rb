81f8bf9048430ecee459da19d3b23057e8ab7c10
Rob McGoye <rob@mcgoye.com>
2021-12-26 13:23:36 -0500

    Adding friends

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
+  get 'my_friends', to: 'users#my_friends'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'  
 end
